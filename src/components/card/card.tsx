ebbe1f31f59828b4cd82b772e84b8462ca65c69d
Keshav Sharma <keshavs8055@gmail.com>
2021-07-28 17:23:35 +0530

    Display user thoughts

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -6,7 +6,6 @@ import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import { Box, Grid, IconButton, Tooltip } from "@material-ui/core";
 import {
-  Edit,
   SubjectOutlined,
   // ThumbUp,
   // ThumbUpAltOutlined,
